a73f6b555c4e8154f3c7cf6c15972f66880b49e2
shakibakram <164419968+shakibakram@users.noreply.github.com>
2024-03-27 23:50:30 +0600

    Delete Tourmate/demo directory

--- Tourmate/demo/src/main/java/Exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package Exception;
-
-public class InvalidCredentialsException extends Throwable{
-    public InvalidCredentialsException(){
-        super("Invalid username and/or password.");
-    }
-}
